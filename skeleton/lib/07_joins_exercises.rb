b434448e8812fd7d01995ae8e6af235fccf591f5
Matt <voorhees.matthew@gmail.com>
2018-09-03 17:59:00 -0700

    start some methods in joins 07

--- skeleton/lib/07_joins_exercises.rb
+++ skeleton/lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+  SELECT
+    movies.title
+  FROM
+    movies
+  JOIN
+    castings ON castings.movie_id = movies.id
+  JOIN
+    actors ON actors.id = castings.actor_id
+  WHERE
+    actors.name = 'Harrison Ford'
   SQL
 end
 
@@ -48,6 +58,16 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+  SELECT
+    movies.title
+  FROM
+    movies
+  JOIN
+    castings ON castings.movie_id = movies.id
+  JOIN
+    actors ON actors.id = castings.actor_id
+  WHERE
+    actors.name = 'Harrison Ford' AND castings.ord != 1
   SQL
 end
 
